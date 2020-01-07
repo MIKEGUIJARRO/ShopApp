e78b72d958192825161371e057719f3233298e55
Mike Guijarro <mguijarro39@gmail.com>
2020-01-07 11:39:57 -0600

    Implementacion de Provider en macro

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screens.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        accentColor: Colors.deepOrange,
-        fontFamily: "Lato"
-
-      ),
-      title: 'Flutter Demo',
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx)=> ProductDetailScreen(),
-
+    return ChangeNotifierProvider(
+      //Ponemos en la raiz de nuestro widget tree ChangeNotifierProvider
+      //nos permitira registrar una clase que podra escuchar en widgets hijos
+      //de esta forma reconstruira estas clases si la data cambia.
+      builder: (ctx) {
+        //Entrega esta clase a los widgets hijos
+        return Products();
       },
+      child: MaterialApp(
+        theme: ThemeData(
+            primarySwatch: Colors.amber,
+            accentColor: Colors.deepOrange,
+            fontFamily: "Lato"),
+        title: 'Flutter Demo',
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+import '../providers/products.dart';
+import './product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    //Se almacena la instancia de la clase
+    final productsData =  Provider.of<Products>(context);
+    /* .of(context) es un metodo generico, por lo tanto me permite indicar con 
+    un tipo generico el tipo de valor que puedo escuchar.
+
+    Le decimos al provider que queremos establecer una linea de comunicacion
+    con la clase <Products> exclusivamente.
+    */
+
+    final products = productsData.items; 
+
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) {
+        return ProductItem(
+          id: products[index].id,
+          title: products[index].title,
+          imageUrl: products[index].imageUrl,
+        );
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
