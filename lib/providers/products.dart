c3b9fddcbc3b9fd9e7b17a364e073239b41948df
Mike Guijarro <a01612042@itesm.mx>
2020-01-19 19:44:35 -0600

    Correccion products - metodo addProduct() necesitava ser modificado para agregar la descripcion

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -61,6 +61,7 @@ class Products with ChangeNotifier {
   void addProduct(Product product) {
     final newProduct = Product(
       title: product.title,
+      description: product.description,
       price: product.price,
       imageUrl: product.imageUrl,
       id: DateTime.now().toString());
