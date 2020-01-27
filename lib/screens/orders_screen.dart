254c33ba6998d272a872cf4bfc182319b4d960ed
Mike Guijarro <a01612042@itesm.mx>
2020-01-27 16:41:42 -0600

    Cambio de orders_screen de Statefull a stateless e implementamos un FutureBuilder()

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,50 +5,44 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-
-class OrdersScreen extends StatefulWidget {
-
+class OrdersScreen extends StatelessWidget {
   static const routenName = "/orders";
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
 
-class _OrdersScreenState extends State<OrdersScreen> {
-
-  var _isLoading = false;
-  
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-  }
   @override
   Widget build(BuildContext context) {
 
-    final orderData = Provider.of<Orders>(context);
+    print("building orders");
+    //final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(title: Text("Your Orders"),),
+      appBar: AppBar(
+        title: Text("Your Orders"),
+      ),
       drawer: AppDrawer(),
-      body: _isLoading ? Center(child: CircularProgressIndicator(),) :ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, index) => OrderItem(
-          order: orderData.orders[index],
-        ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              //Do handling Error here.
+              return Center(
+                child: Text("An error ocurred!"),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, index) => OrderItem(
+                          order: orderData.orders[index],
+                        )),
+              );
+            }
+          }
+        },
       ),
     );
   }
